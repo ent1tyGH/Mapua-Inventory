e4fa7674f13452f7eb6c9a204d9148f26cf61294
ent1tyGH <mangahisg@gmail.com>
2025-09-19 16:51:41 +0800

    removed non-functional frontend

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,9 @@
 import { Routes } from '@angular/router';
 import { EquipmentTypeComponent } from './components/equipment-type/equipment-type.component';
 import { ItemComponent } from './components/item/item.component';
-import { BorrowComponent } from './components/borrow/borrow.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/items', pathMatch: 'full' },
   { path: 'items', component: ItemComponent },
-  { path: 'borrow/:id', component: BorrowComponent },
   { path: 'equipment-type/add', component: EquipmentTypeComponent }
 ];
