a09e1794964ea3db6d87a2bbb8de7469e5079523
ent1tyGH <mangahisg@gmail.com>
2025-10-07 22:25:08 +0800

    added feature: export database to excel file

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RouterModule } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule],
+  imports: [RouterOutlet, RouterModule, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'frontend';
+  constructor(private http: HttpClient) {}
+  title = 'DOIT Inventory System';
+  downloadExcel() {
+      this.http.get('http://localhost:8080/api/borrow-records/export', { responseType: 'blob' })
+        .subscribe((blob) => {
+          const url = window.URL.createObjectURL(blob);
+          const a = document.createElement('a');
+          a.href = url;
+          a.download = 'Inventory_Report.xlsx';
+          a.click();
+          window.URL.revokeObjectURL(url);
+        });
+  }
 }
