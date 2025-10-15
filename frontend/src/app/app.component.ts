897be110267dae7b8b90a1bd25110f464b908997
ent1tyGH <mangahisg@gmail.com>
2025-10-16 01:58:43 +0800

    added all tables in backup to csv

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -13,15 +13,15 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 export class AppComponent {
   constructor(private http: HttpClient) {}
   title = 'DOIT Inventory System';
-  downloadExcel() {
-      this.http.get('http://localhost:8080/api/borrow-records/export', { responseType: 'blob' })
-        .subscribe((blob) => {
-          const url = window.URL.createObjectURL(blob);
-          const a = document.createElement('a');
-          a.href = url;
-          a.download = 'Inventory_Report.xlsx';
-          a.click();
-          window.URL.revokeObjectURL(url);
-        });
+  downloadBackup() {
+    this.http.get('http://localhost:8080/backup/download', { responseType: 'blob' }).subscribe((data) => {
+      const blob = new Blob([data], { type: 'application/zip' });
+      const url = window.URL.createObjectURL(blob);
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = 'database_backup.zip';
+      a.click();
+      window.URL.revokeObjectURL(url);
+    });
   }
 }
